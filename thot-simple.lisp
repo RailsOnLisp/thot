5bc99df995ab76d467e6f427c95019a98cf31048
Thomas de Grivel <thoxdg@gmail.com>
2017-06-27 18:06:44 +0200

    support optional self-pipe in acceptor loop

--- thot-simple.lisp
+++ thot-simple.lisp
@@ -1,16 +1,21 @@
 
 (in-package :thot)
 
-(defun acceptor-loop-simple (fd)
+(defun acceptor-loop-simple (fd &optional pipe)
   (declare (type (unsigned-byte 31) fd))
-  (labels ((acceptor-loop-simple-fun ()
-             (loop
-                (when *stop*
-                  (return))
-                (ignore-errors
-                  (unistd:with-selected (`(,fd) () () 1)
+  (let ((readfds))
+    (push fd readfds)
+    (when pipe
+      (push pipe readfds))
+    (labels ((acceptor-loop-simple-fun ()
+               (loop
+                  (when *stop*
+                    (return))
+                  (unistd:with-selected (readfds () () 100)
                       (readable writable errors)
-                    (when readable
+                    (when (and pipe (find pipe readable))
+                      (return))
+                    (when (find fd readable)
                       (socket:with-accept (clientfd) fd
                         (with-stream (stream (babel-io-stream
                                               (fd-io-stream clientfd)))
